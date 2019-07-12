f4b6e6f33d4b2ac96b9c31d452cd5694e3496c53
Nguyen Van Thieu <thieu-nguyen-niteco@outlook.com>
2019-07-12 10:00:09 +0700

    Add default data again

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import App from './src/App';
 import { name as appName } from './app.json';
 import React from 'react';
 import { YellowBox, AppRegistry } from 'react-native';
-YellowBox.ignoreWarnings(['Warning: componentWillMount is deprecated', 'Warning: componentWillReceiveProps is deprecated']);
+YellowBox.ignoreWarnings([
+	'Warning: componentWillMount is deprecated', 
+	'Warning: componentWillReceiveProps is deprecated',
+	'RCTBridge'
+]);
 
 export default class ToDo extends React.Component {
   render() {
--- src/redux/reducer/arrWorksReducer.js
+++ src/redux/reducer/arrWorksReducer.js
@@ -1,18 +1,8 @@
 const defaultArrWorks = [
-        { id: 1, title: 'iOS', detail: 'Objective-C, Swift', isDone: true, isShow: false },
-        { id: 2, title: 'Android', detail: 'Java, Kotlin', isDone: false, isShow: false },
-        { id: 3, title: 'React Native', detail: 'JavaScript', isDone: true, isShow: false },
-        { id: 4, title: 'Flutter', detail: 'Dark', isDone: true, isShow: false },
-        { id: 5, title: 'Unity', detail: 'JavaScript, C#', isDone: false, isShow: false },
-        { id: 6, title: 'PHP', detail: 'PHP, MySQL', isDone: true, isShow: false },
-        { id: 7, title: 'Java', detail: 'Java', isDone: false, isShow: false },
-        { id: 8, title: '.NET', detail: 'C#, VB.NET, C++', isDone: true, isShow: false },
-        { id: 9, title: 'Front End', detail: 'HTML, CSS, JavaScript', isDone: true, isShow: false },
-        { id: 10, title: 'Back End', detail: 'PHP, .NET, Java', isDone: false, isShow: false },
-        { id: 11, title: 'Database', detail: 'MySQL, SQL Server, SQLite...', isDone: true, isShow: false },
-        { id: 12, title: 'Tester', detail: 'Unit Test, UI Test', isDone: false, isShow: false },
-        { id: 13, title: 'HR', detail: 'Job, Interview', isDone: true, isShow: false },
-        { id: 14, title: 'PM', detail: 'Project, Manager', isDone: false, isShow: false }
+        { id: 1, title: 'Simple App', detail: 'React Native', isDone: true, isShow: false },
+        { id: 2, title: 'Simple Redux', detail: 'React Native', isDone: true, isShow: false },
+        { id: 3, title: 'To Do', detail: 'React Native', isDone: false, isShow: false },
+        { id: 4, title: 'To Do List', detail: 'React Native', isDone: false, isShow: false }
 ];
 
 const arrWorksReducer = (state = defaultArrWorks, action) => {
